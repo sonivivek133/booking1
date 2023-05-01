9123dbc35cc024e6d0b1d13f868c124472d7f3fa
vivek <sonivivek133@gmail.com>
2023-05-02 01:51:10 +0530

    updated

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -10,7 +10,7 @@ const Datatable = ({columns}) => {
   const location = useLocation();
   const path = location.pathname.split("/")[1];
   const [list, setList] = useState("");
-  const { data, loading, error } = useFetch(`/${path}`);
+  const { data } = useFetch(`/${path}`);
 
   useEffect(() => {
     setList(data);
